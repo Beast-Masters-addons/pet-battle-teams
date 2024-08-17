658e10bbc421e9c18d37265f2738fc1a3b518538
datagutten <datagutten@datagutten.net>
2024-08-17 17:17:15 +0200

    Add compatibility module for EasyMenu

--- get_libs.sh
+++ get_libs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh | bash -s -- -g retail -c -d -z
-mv -f .release/PetBattleTeams/libs/* PetBattleTeams
+rsync -a --del .release/PetBattleTeams/libs PetBattleTeams
